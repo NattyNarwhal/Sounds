7f1c90a570d22dcd3dc885e7523ca98337a003b6
Calvin <calvin@openmailbox.org>
2017-08-20 20:22:26 -0300

    Update assembly info

--- Sounds/Properties/AssemblyInfo.cs
+++ Sounds/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sounds")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sounds music player")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("cmpct")]
 [assembly: AssemblyProduct("Sounds")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("MIT license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
