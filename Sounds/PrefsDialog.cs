69f6e8cc28acc7494d4ef4df4a9d6c637ca367fe
Calvin <calvin@openmailbox.org>
2017-08-22 20:17:39 -0300

    Expose the properties on prefs dialog

--- Sounds/PrefsDialog.cs
+++ Sounds/PrefsDialog.cs
@@ -12,6 +12,90 @@ namespace Sounds
 {
     public partial class PrefsDialog : Form
     {
+        public bool ShowToolbar
+        {
+            get
+            {
+                return showToolbarBox.Checked;
+            }
+            set
+            {
+                showToolbarBox.Checked = value;
+            }
+        }
+
+        public bool ShowStatusbar
+        {
+            get
+            {
+                return showStatusbarBox.Checked;
+            }
+            set
+            {
+                showStatusbarBox.Checked = value;
+            }
+        }
+
+        public bool ShowInfoPane
+        {
+            get
+            {
+                return showInfoPaneBox.Checked;
+            }
+            set
+            {
+                showInfoPaneBox.Checked = value;
+            }
+        }
+
+        public bool DeleteOnTrackChange
+        {
+            get
+            {
+                return deleteOnTrackChangeBox.Checked;
+            }
+            set
+            {
+                deleteOnTrackChangeBox.Checked = value;
+            }
+        }
+
+        public bool Recursive
+        {
+            get
+            {
+                return recursiveBox.Checked;
+            }
+            set
+            {
+                recursiveBox.Checked = value;
+            }
+        }
+
+        public int VolumneIncrement
+        {
+            get
+            {
+                return Convert.ToInt32(volIncrementBox.Value);
+            }
+            set
+            {
+                volIncrementBox.Value = value;
+            }
+        }
+
+        public int TimeIncrement
+        {
+            get
+            {
+                return Convert.ToInt32(timeIncrementBox.Value);
+            }
+            set
+            {
+                timeIncrementBox.Value = value;
+            }
+        }
+
         public PrefsDialog()
         {
             InitializeComponent();
